6772051d0f465b718784d2c1b0639286214abf5b
verozacarias-prog <vero.zacarias@gmail.com>
2026-03-10 17:24:46 -0300

    feat: add loging csvv

--- classifier.go
+++ classifier.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
+	"log"
+	"encoding/csv"
+	"os"
 
 	openai "github.com/openai/openai-go/v3"
 )
@@ -18,8 +22,9 @@ type ClassifyResponse struct {
 	TokensUsed int64  `json:"tokens_used"`
 }
 
+var openAIClient = openai.NewClient()
+
 func Classify(request ClassifyRequest) (ClassifyResponse, error) {
-	client := openai.NewClient()
 
 	messages := []openai.ChatCompletionMessageParamUnion{
 		openai.SystemMessage(`Sos un clasificador de mensajes para una peluquería.
@@ -37,7 +42,7 @@ No agregues explicaciones. No salgas del JSON.`),
 		openai.UserMessage(request.Message),
 	}
 
-	resp, err := client.Chat.Completions.New(context.Background(), openai.ChatCompletionNewParams{
+	resp, err := openAIClient.Chat.Completions.New(context.Background(), openai.ChatCompletionNewParams{
 		Model:       openai.ChatModelGPT4oMini,
 		Messages:    messages,
 		Temperature: openai.Float(0),
@@ -54,5 +59,29 @@ No agregues explicaciones. No salgas del JSON.`),
 		return ClassifyResponse{}, fmt.Errorf("error parseando respuesta: %w", err)
 	}
 	result.TokensUsed = resp.Usage.TotalTokens
+
+	WriteCSVLog(request.Message, result)
 	return result, nil
+}
+
+func WriteCSVLog(message string, result ClassifyResponse) {
+	csvFile, err := os.OpenFile("logs.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("error al abrir el archivo CSV: %v", err)
+	}
+	defer csvFile.Close()
+
+	csvWriter := csv.NewWriter(csvFile)
+	csvWriter.Write([]string{
+		time.Now().Format(time.RFC3339),
+		message, 
+		result.Intent, 
+		result.Confidence, 
+		fmt.Sprintf("%d", result.TokensUsed)})
+	
+	csvWriter.Flush()
+	
+	if err := csvWriter.Error(); err != nil {
+		log.Fatalf("error al escribir en el archivo CSV: %v", err)
+	}
 }
